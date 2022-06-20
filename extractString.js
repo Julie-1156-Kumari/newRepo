b6c25c4374dac1e32711b0c24f651a6f7342ecf2
Julie-1156-Kumari <chawla1julie@gmail.com>
2022-06-20 12:29:03 +0530

    changes in extracting files

--- extractString.js
+++ extractString.js
@@ -1,2 +1,15 @@
+//slice(start, end);
+
 str = "Apple, Banana, Kiwi";
 console.log(str.slice(-16, -3));
+
+//JavaScript counts positions from zero.
+//First position is 0.
+//Second position is 1.
+//If a parameter is negative, the position is counted from the end of the string.
+
+console.log(str.slice(6)); //slice out the rest of the string
+
+console.log.(str.slice(-12)); //or, counting from the end
+
+
